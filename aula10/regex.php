2ad55d46eb80935f326901a21b43d243cee7822e
isernhagen <ricardo.isernhagen@gmail.com>
2013-04-20 14:58:56 -0400

    Regex e preg_match()

---
+++ aula10/regex.php
@@ -0,0 +1,16 @@
+<?php
+
+$pattern = "/(?P<username>[a-z]+)@(?<domain>[a-z]+\.([a-z]{2,3}\.[a-z]{2,3})/";
+
+$patern = "/[rR]icardo [aA]ugusto [iI]senhagen/"	
+
+$subject = "ricardo augusto isernhagen";
+
+$r = preg_match($pattern, $subject, $matches);
+
+echo "<pre>";
+var_dump($r);
+print_r($matches);
+
+
+?>
